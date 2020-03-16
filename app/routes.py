a8e87592890cf20bdd9da30347e9cda5a718bb57
Chirag Soni <chiragsoni81245@gmail.com>
2020-03-17 02:42:15 +0530

    replace threading with multiprocessing

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,11 @@ from flask import Flask,render_template,jsonify,url_for,request
 from app.set_pointer import *
 from app.utility_function import *
 from collections import OrderedDict
-from threading import Thread,Timer
 import time
 from app import app
-import threading
 from datetime import datetime
+from multiprocessing import Process
+import multiprocessing
 
 handles = ['ziddi', 'tanu38', 'chiragsoni812', 'mohi07', 'chauhan002', 'shivambhat', 'sp_', 'Nishant_Rao', 'Aka_coder', 'hr4_harsh', 'AM_coder', 'ankitkochar1578', '_viru_', 'hitzmac', 'khiladi.07', '_chanchal', "shubhamz950"]
 
@@ -45,19 +45,32 @@ def info(user):
 @app.route("/alpha")
 def reload():
 	flag=0
-	for i in threading.enumerate()[1:]:
+	for i in multiprocessing.active_children():
 		if i.name=="reloader":
 			flag=1
 	if flag==1:
 		return "<html><h1>Already Running</h1></html>"
 	else:
-		Thread(name="reloader",target=reloading,args=(handles,)).start()
+		# Thread(name="reloader",target=reloading,args=(handles,)).start()
+		Process(name="reloader",target=reloading,args=(handles,)).start()
 		return "<html><h1>Reloading Started</h1></html>"
+
+@app.route("/salpha")
+def stop_reload():
+	flag=0
+	for i in multiprocessing.active_children():
+		if i.name=="reloader":
+			flag=1
+			i.terminate()
+	if flag==1:
+		return "<html><h1>Reloader Stoped</h1></html>"
+	else:
+		return "<html><h1>Roloader is already stoped</h1></html>"
 
 @app.route("/status_reloader")
 def status_reloader():
 	flag=0
-	for i in threading.enumerate()[1:]:
+	for i in multiprocessing.active_children():
 		if i.name=="reloader":
 			flag=1
 	if flag==1:
